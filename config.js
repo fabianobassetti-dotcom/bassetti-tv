eb9e4031b0fe55b856de9a47f998f23e8790a9e5
hub-fb <fabiano.bassetti@gmail.com>
2026-07-06 16:36:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
     PLAYLIST_URL: "https://raw.githubusercontent.com/hub-fb/minha-playlist-iptv/refs/heads/main/CanaisBR.m3u",
-    LOG_PREFIX: "[Bassetti TV]"
+    LOG_PREFIX: "[Player TV]"
 };
